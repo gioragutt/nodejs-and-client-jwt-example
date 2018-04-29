77f5a382bacca170b250e9f6913821eaca71373f
Giora Guttsait <giora111@gmail.com>
2018-04-30 00:54:28 +0300

    More project setup

--- packages/ngrx-client/src/environments/environment.ts
+++ packages/ngrx-client/src/environments/environment.ts
@@ -3,6 +3,10 @@
 // `ng build --env=prod` then `environment.prod.ts` will be used instead.
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
+const serverUrl = 'http://localhost:3000';
+
 export const environment = {
-  production: false
+  production: false,
+  serverUrl,
+  apiBaseUrl: `${serverUrl}/api/v1`,
 };
